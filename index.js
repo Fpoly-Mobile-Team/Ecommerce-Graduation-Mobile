985ab1f1d0bb007fcbfb88d64c762be7edf1f818
Huy <huytdps13400@fpt.edu.vn>
2021-05-13 11:28:58 +0700

    lib navigation and bottom

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
+import 'react-native-gesture-handler';
 import App from './App';
 import {name as appName} from './app.json';
-
+LogBox.ignoreAllLogs();
 AppRegistry.registerComponent(appName, () => App);
