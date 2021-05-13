d2d005752afa0b879fb8d15fb830ae199201cbad
Huy <huytdps13400@fpt.edu.vn>
2021-05-13 11:23:53 +0700

    tips folder

---
+++ src/theme/index.js
@@ -0,0 +1,16 @@
+export const colors = {
+  orange: '#FE930F',
+  lightGray: '#A5A5A5',
+  gray: '#424242',
+  smoke: '#F8EFFB',
+  white: '#FFFFFF',
+  black: '#000000',
+  placeholder: '#707070',
+  blue: '#0972FF',
+  yellow: '#FFD500',
+  red: '#EB0505',
+  gradient: ['#FFC76F', '#FE9C21', '#FD8B00'],
+  gradient_ios: ['#eaafc8', '#654ea3'],
+  dark_purple: '#2F0B3A',
+  purple: '#654ea3',
+};
