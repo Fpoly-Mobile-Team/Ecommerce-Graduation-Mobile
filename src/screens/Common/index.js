3c1809a7f146d029d8540373c33b0350d2328103
unknown <danhdtps13304@fpt.edu.vn>
2021-07-10 15:53:59 +0700

    fix

--- src/screens/Common/index.js
+++ src/screens/Common/index.js
@@ -1,5 +1,6 @@
 import ProductDetails from './ProductDetails';
 import CartScreens from './CartScreens';
+import SearchScreen from '.'
 export const common = {
   PRODUCT_DETAILS: ProductDetails,
   CARTSCREENS: CartScreens,
