d413a30fccf8a8d913f9a907d7bfa9b52b9a5032
Huy <huytdps13400@fpt.edu.vn>
2021-07-15 15:37:01 +0700

    update

--- src/screens/Bottom/ProfileScreens/index.js
+++ src/screens/Bottom/ProfileScreens/index.js
@@ -1,10 +1,12 @@
 import {Block, Text} from '@components';
 import React from 'react';
 import {StyleSheet} from 'react-native';
+import HeaderProflie from './components/HeaderProflie';
 
 const ProfileScreens = () => {
   return (
     <Block>
+      <HeaderProflie />
       <Text>ProfileScreens</Text>
     </Block>
   );
