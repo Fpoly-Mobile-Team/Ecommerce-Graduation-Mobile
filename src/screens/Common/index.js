15f96af399ceb7e816ffa7db77d66536ebb051c3
huytdps13400 <kingonwork@gmail.com>
2021-08-02 00:05:00 +0700

    update

--- src/screens/Common/index.js
+++ src/screens/Common/index.js
@@ -1,7 +1,9 @@
 import CartScreens from './CartScreens';
 import ProductDetails from './ProductDetails';
+import PaymentScreen from './CartScreens/navigation/PaymentScreen';
 
 export const common = {
   PRODUCT_DETAILS: ProductDetails,
   CARTSCREENS: CartScreens,
+  PAYMENT_SCREEN: PaymentScreen,
 };
