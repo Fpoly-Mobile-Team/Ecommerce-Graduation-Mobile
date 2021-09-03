7ecb3ab4f0304793211233bca2040ef80c1fe40e
Dinh Tien Phong <phongdtps13338@fpt.edu.vn>
2021-09-03 23:09:24 +0700

    auth-forgot-ui

--- src/screens/auth/ForgotPassword/index.js
+++ src/screens/auth/ForgotPassword/index.js
@@ -1,81 +1,64 @@
-import {Block, Button, Text} from '@components';
+import {Block, Button, Text, TextInput} from '@components';
+import CheckBox from '@components/CheckBox';
 import {useNavigation} from '@react-navigation/native';
 import {theme} from '@theme';
 import React, {useState} from 'react';
-import {Pressable, StatusBar, TextInput} from 'react-native';
+import { Pressable, StatusBar } from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import styles from './styles';
 
-const Forgot = () => {
+const Register = ({callBack}) => {
   const [email, setEmail] = useState();
   const navigation = useNavigation();
   const {top} = useSafeAreaInsets();
 
   return (
-    <Block flex backgroundColor="background">
+    <Block flex backgroundColor="background" paddingTop={top + 40}>
       <StatusBar barStyle="dark-content" translucent animated />
-      <Block flex paddingTop={top + 40}>
-        <Block paddingBottom={66} paddingHorizontal={12}>
-          <Text size={32} fontType="semibold">
-            Quên mật khẩu
-          </Text>
-        </Block>
-        <Block paddingHorizontal={12} paddingBottom={16}>
-          <Text size={13}>
-            Please, enter your email address. You will receive a link to create
-            a new password via email.
-          </Text>
-        </Block>
-        <Block
+      <Block marginBottom={56} paddingHorizontal={12}>
+        <Text size={32} fontType="semibold">
+          Quên mật khẩu
+        </Text>
+      </Block>
+      <Block paddingHorizontal={12} paddingBottom={16}>
+        <Text size={13}>
+          Please, enter your email address. You will receive a link to create a
+          new password via email.
+        </Text>
+      </Block>
+      <Block paddingHorizontal={12}>
+        <TextInput
+          label="Email"
+          containerInputStyle={styles.containerInputStyle}
+          labelStyle={styles.label}
+          inputStyle={styles.inputStyle}
+          keyboardType="email-address"
+          placeholder="Nhập email cần xác nhận"
+          onChangeText={text => setEmail(text)}
+        />
+      </Block>
+      <Block marginTop={20} paddingHorizontal={12}>
+        <Button
+          title="GỞI XÁC NHẬN"
+          height={50}
           shadow
-          shadowColor={theme.colors.black}
-          elevation={0.8}
-          flex
-          flexGrow
-          marginHorizontal={12}
-          radius={5}
-          marginTop={5}
-          marginBottom={8}
-          paddingVertical={5}
-          paddingHorizontal={12}
-          backgroundColor={theme.colors.white}>
-          <Block paddingTop={5}>
-            <Text style={styles.textFilter} size={11}>
-              Tên đăng nhập
-            </Text>
-          </Block>
-          <Block paddingBottom={0}>
-            <TextInput
-              onChangeText={text => setEmail(text)}
-              keyboardType="email-address"
-              placeholder="ken.phong1802@gmail.com"
-              placeholderTextColor={theme.colors.placeholder}
-              style={styles.label}
-            />
-          </Block>
-        </Block>
-
-        <Block row justifyEnd alignCenter marginHorizontal={12} paddingTop={16}>
-          <Pressable>
-            <Text style={styles.textForgot} size={14} paddingHorizontal={3}>
-              Đăng ký tài khoản?
-            </Text>
-          </Pressable>
-        </Block>
-
-        <Block marginTop={32} paddingHorizontal={12} marginBottom={1000}>
-          <Button
-            title="GỞI XÁC NHẬN"
-            height={50}
-            style={{borderRadius: 5}}
-            backgroundColor={theme.colors.pink}
-            shadowColor={`${theme.colors.pink}20`}
-            elevation={10}
-          />
-        </Block>
+          backgroundColor={theme.colors.pink}
+          shadowColor={`${theme.colors.pink}80`}
+          elevation={10}
+          style={{borderRadius: 5}}
+        />
+      </Block>
+      <Block row marginTop={30} alignCenter justifyCenter>
+        <Text color={theme.colors.black}>Bạn đã nhớ tài khoản? </Text>
+        <Pressable onPress={() => navigation.goBack()}>
+          <Text color={theme.colors.pink} fontType="bold">
+            {' '}
+            Đăng nhập
+          </Text>
+        </Pressable>
       </Block>
     </Block>
   );
 };
 
-export default Forgot;
+export default Register;
