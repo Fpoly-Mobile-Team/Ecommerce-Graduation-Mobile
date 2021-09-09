b982c6f1e489ebf64e7e71e5d796235cad529b12
TĂNG DƯƠNG GIA ĐẠT <71257408+tangduonggiadat@users.noreply.github.com>
2021-09-09 17:06:01 +0700

    Code Chat Screeen and Message use gitfed chat library

--- src/screens/Bottom/ChatScreens/index.js
+++ src/screens/Bottom/ChatScreens/index.js
@@ -2,15 +2,36 @@ import {Block, Header} from '@components';
 import {theme} from '@theme';
 import React from 'react';
 import {StyleSheet} from 'react-native';
+import styles from './styles';
+import {Searchbar} from 'react-native-paper';
+import {Search} from '@assets/svg/common';
+import ListMess from './components/ListMessage';
 
 const ChatScreens = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const onChangeSearch = query => {
+    setSearchQuery(query);
+    handlerSearch(query);
+  };
   return (
-    <Block flex backgroundColor={theme.colors.white}>
+    <Block flex backgroundColor={theme.colors.background}>
       <Header title="Tin nhắn" />
+
+      <Block backgroundColor={theme.colors.white}>
+        <Searchbar
+          icon={Search}
+          style={styles.searchStyle}
+          inputStyle={styles.inputStyle}
+          placeholder={'Tìm kiếm'}
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+      </Block>
+      <Block flex>
+        <ListMess />
+      </Block>
     </Block>
   );
 };
 
 export default ChatScreens;
-
-const styles = StyleSheet.create({});
