0bcb0c9230369ebd403beefaaa941ca0c9996184
huytdps13400 <kingonwork@gmail.com>
2021-09-18 23:53:18 +0700

    update code

---
+++ src/components/Common/ItemList/ItemShop/styles.js
@@ -0,0 +1,19 @@
+import {getSize} from '@utils/responsive';
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  icontick: {
+    width: getSize.s(14),
+    height: getSize.s(14),
+  },
+  stylebtn: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconnext: {
+    width: getSize.s(10),
+    height: getSize.s(10),
+    marginLeft: getSize.m(2),
+  },
+});
