3b93d03775311d6d532485578f1ffc488640b25b
huytdps13400 <kingonwork@gmail.com>
2021-09-19 21:06:23 +0700

    pushcode

--- src/components/Common/ItemList/ItemPromoScreen/index.js
+++ src/components/Common/ItemList/ItemPromoScreen/index.js
@@ -1,8 +1,7 @@
+import {Button} from '@components';
+import {getSize} from '@utils/responsive';
 import React from 'react';
-import {theme} from '@theme';
-import {getSize, width} from '@utils/responsive';
-import {Block} from '@components';
-import {Image, View, Pressable, Text} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import styles from './styles';
 
 const ItemPromoScreen = ({name, title, date, image}) => {
@@ -10,7 +9,12 @@ const ItemPromoScreen = ({name, title, date, image}) => {
     <View style={styles.ItemContainer}>
       <View style={styles.ItemCart}>
         <View style={styles.ItemTop}>
-          <View style={{width: '20%', height: '100%', borderRadius: 8}}>
+          <View
+            style={{
+              width: '20%',
+              height: '100%',
+              borderRadius: 8,
+            }}>
             <View style={{alignItems: 'center', top: 21}}>
               <Image
                 source={{
@@ -24,8 +28,7 @@ const ItemPromoScreen = ({name, title, date, image}) => {
           <View style={{width: '80%', height: '100%', borderRadius: 8}}>
             <View style={{top: 16}}>
               <Text style={styles.textDate}>{name}</Text>
-              <Text
-                style={{fontWeight: 'bold', fontSize: 14, fontStyle: 'Inter'}}>
+              <Text style={{fontWeight: '500', fontSize: 14, marginTop: 5}}>
                 {title}
               </Text>
             </View>
@@ -33,26 +36,22 @@ const ItemPromoScreen = ({name, title, date, image}) => {
         </View>
         <View style={styles.ItemBottom}>
           <View style={styles.ItemText}>
-            <Text style={styles.textDate}>HSD:</Text>
-            <Text style={styles.textDate}>{date}</Text>
+            <Text style={styles.textDate}>HSD: {date}</Text>
           </View>
-          <View style={{paddingHorizontal: 21}}>
-            <Pressable
-              style={{
-                backgroundColor: '#3B5998',
-                width: 46,
-                height: 28,
-                borderRadius: 4,
-                paddingHorizontal: 10,
-                paddingVertical: 4,
-              }}>
-              <Text style={{color: 'white'}}>Lưu</Text>
-            </Pressable>
-          </View>
+          <Button
+            title="Lưu"
+            height={28}
+            width={46}
+            style={{
+              backgroundColor: '#3B5998',
+              borderRadius: getSize.s(4),
+              marginRight: getSize.s(16),
+            }}
+          />
         </View>
 
         <View style={styles.ItemIconRight} />
-        <View></View>
+
         <View style={styles.ItemIconLeft} />
       </View>
     </View>
--- src/screens/Common/navigations/PromoScreen/index.js
+++ src/screens/Common/navigations/PromoScreen/index.js
@@ -1,72 +1,79 @@
-import {Block, Header,Text} from '@components';
-import {useNavigation} from '@react-navigation/core';
-import React, {useState} from 'react';
-import {theme} from '@theme';
+import {Block, Header, Text} from '@components';
 import ItemPromoScreen from '@components/Common/ItemList/ItemPromoScreen';
-import { FlatList } from 'react-native-gesture-handler';
-import {data} from './components/data'
-import OptionsMenu from "react-native-option-menu";
-import {SelectCircle,SelectsCircle} from '../../../../assets/svg/common';
-import { ToastAndroid } from 'react-native';
-const select_icon = require("../../../../assets/icons/Shape.png");
+import {useNavigation} from '@react-navigation/core';
+import {theme} from '@theme';
+import {Toast} from '@utils/helper';
+import React from 'react';
+import {FlatList} from 'react-native-gesture-handler';
+import OptionsMenu from 'react-native-option-menu';
+import {SelectCircle, SelectsCircle} from '../../../../assets/svg/common';
+import {data} from './components/data';
+
+const select_icon = require('../../../../assets/icons/Shape.png');
 
 const PromoScreen = () => {
-    const navigation = useNavigation();
-    const editPost = () => {
-      ToastAndroid.show('Edit Post', ToastAndroid.SHORT);
-    };
-    const deletePost = () => {
-      ToastAndroid.show('Delete Post', ToastAndroid.SHORT);
-    };
-    const Select = (<SelectsCircle/>)
+  const navigation = useNavigation();
+  const editPost = () => {
+    Toast('Edit Post');
+  };
+  const deletePost = () => {
+    Toast('Delete Post');
+  };
+  const Select = <SelectsCircle />;
 
-    const renderItem = ({item}) =>(
-        <ItemPromoScreen
-            name={item.name}
-            title={item.title}
-            date={item.date}
-            image={item.Image}
-        />
-
-    );
+  const renderItem = ({item}) => (
+    <ItemPromoScreen
+      name={item.name}
+      title={item.title}
+      date={item.date}
+      image={item.Image}
+    />
+  );
 
   return (
-    <Block flex backgroundColor="background" >
-      <Header checkBackground  canGoBack title="Mã khuyến mãi" />
-     <Block paddingHorizontal={12} paddingVertical={12} row backgroundColor={theme.colors.white} > 
-       <SelectCircle/>
-       <Block paddingLeft={8} justifyCenter>
-         <Text size={16} color={theme.colors.placeholder} >Sắp xếp:</Text>
-       </Block>
-       <Block paddingHorizontal={3} justifyCenter>
-         <Text size={16} color={theme.colors.black}>Tất cả</Text>
-        
-       </Block>
-       <Block paddingHorizontal={1} justifyCenter alignCenter>
-       
-       <OptionsMenu
-              button={select_icon}
-              buttonStyle={{
-                width: 10,
-                height: 12,
-                resizeMode: "contain",
-                
-              }}
-              destructiveIndex={1}
-              options={['Hạn sử dụng','Tất cả']}
-              actions={[editPost, deletePost]}
-            />
-            {/* <SelectsCircle/> */}
-       </Block>
-     </Block>
-   
+    <Block flex backgroundColor="#E9EAEB">
+      <Header checkBackground canGoBack title="Mã khuyến mãi" />
+      <Block
+        paddingHorizontal={12}
+        paddingVertical={12}
+        row
+        backgroundColor={theme.colors.white}>
+        <SelectCircle />
+        <Block paddingLeft={8} justifyCenter>
+          <Text
+            size={16}
+            color={theme.colors.placeholder}
+            fontType="semibold"
+            lineHeight={18}>
+            Sắp xếp:
+          </Text>
+        </Block>
+        <Block paddingHorizontal={3} justifyCenter>
+          <Text size={16} color={theme.colors.black} lineHeight={18}>
+            Tất cả
+          </Text>
+        </Block>
+        <Block paddingHorizontal={1} justifyCenter alignCenter>
+          <OptionsMenu
+            button={select_icon}
+            buttonStyle={{
+              width: 10,
+              height: 12,
+              resizeMode: 'contain',
+            }}
+            destructiveIndex={1}
+            options={['Hạn sử dụng', 'Tất cả']}
+            actions={[editPost, deletePost]}
+          />
+        </Block>
+      </Block>
+
       <FlatList
-          data={data}
-          renderItem={renderItem}
-          keyExtractor={item=> item.id.toString()}
-          showsVerticalScrollIndicator={false}
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+        showsVerticalScrollIndicator={false}
       />
-   
     </Block>
   );
 };
