1b191fe009cb5226f28d66dd8695836f2941318f
TĂNG DƯƠNG GIA ĐẠT <71257408+tangduonggiadat@users.noreply.github.com>
2021-11-24 18:55:28 +0700

    Update styles.js

--- src/screens/Common/ProductStore/navigation/StoreDenounce/styles.js
+++ src/screens/Common/ProductStore/navigation/StoreDenounce/styles.js
@@ -4,7 +4,7 @@ import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
   avatar: {
     width: getSize.s(56),
-    height: getSize.v(56),
+    height: getSize.s(56),
     borderRadius: getSize.s(100) / 2,
     borderWidth: 0.5,
   },
