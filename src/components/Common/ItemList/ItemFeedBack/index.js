84fccd487d50d428551bd2eae7614996fc273df7
TĂNG DƯƠNG GIA ĐẠT <71257408+tangduonggiadat@users.noreply.github.com>
2021-11-27 19:51:16 +0700

    Update code

--- src/components/Common/ItemList/ItemFeedBack/index.js
+++ src/components/Common/ItemList/ItemFeedBack/index.js
@@ -1,12 +1,41 @@
 import {Block, Text} from '@components';
 import {theme} from '@theme';
 import {getSize} from '@utils/responsive';
-import React from 'react';
-import {Image} from 'react-native';
+import React, {useState, useEffect, Fragment} from 'react';
+import {Image, Pressable, Modal} from 'react-native';
 import {Rating} from 'react-native-elements';
+import ImageViewer from 'react-native-image-zoom-viewer';
 import styles from './styles';
 
 const ItemFeedBack = ({name, avatar, star, description, time, image}) => {
+  const [ImageViwerIsVisible, showImageViwer] = useState(false);
+  let [viewingIndex, setViewingIndex] = useState(-1);
+  useEffect(() => {
+    viewingIndex !== -1 && showImageViwer(true);
+  }, [viewingIndex]);
+  useEffect(() => {
+    !ImageViwerIsVisible && setViewingIndex(-1);
+  }, [ImageViwerIsVisible]);
+
+  const renderImage = (item, index) => {
+    return (
+      <Pressable
+        style={styles.wrapper}
+        key={item}
+        onPress={() => setViewingIndex(index)}>
+        <Image
+          key={index}
+          source={{uri: item}}
+          style={{
+            ...styles.imglogo,
+            marginBottom: getSize.m(5),
+            marginRight: getSize.m(5),
+          }}
+        />
+      </Pressable>
+    );
+  };
+
   return (
     <Block marginBottom={24}>
       <Block row space="between">
@@ -36,33 +65,24 @@ const ItemFeedBack = ({name, avatar, star, description, time, image}) => {
           <Text size={13} marginVertical={5} justify>
             {description}
           </Text>
-          <Block flex wrap row>
-            {image?.map(renderImage)}
-          </Block>
-
-          <Block
-            marginTop={12}
-            marginHorizontal={-200}
-            backgroundColor={'#EBF0FF'}
-            height={1}
-            borderRadius={5}></Block>
+          <Fragment>
+            {!!ImageViwerIsVisible && (
+              <Modal
+                transparent={true}
+                onRequestClose={() => showImageViwer(false)}>
+                <ImageViewer
+                  imageUrls={image.map(f => ({url: f}))}
+                  index={viewingIndex}
+                />
+              </Modal>
+            )}
+            <Block flex wrap row>
+              {image?.map(renderImage)}
+            </Block>
+          </Fragment>
         </Block>
       </Block>
     </Block>
-  );
-};
-
-const renderImage = (item, index) => {
-  return (
-    <Image
-      key={index}
-      source={{uri: item}}
-      style={{
-        ...styles.imglogo,
-        marginBottom: getSize.m(5),
-        marginRight: getSize.m(5),
-      }}
-    />
   );
 };
 
